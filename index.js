0fa92cd3c2cc6c8ab1238ec28b246c85f24f99a6
Behuamuh <sven1980@list.ru>
2020-07-03 05:15:49 +0300

    feat: added capitalize & todo count

--- index.js
+++ index.js
@@ -1,78 +1,103 @@
 const todos = [
-  'Сделать проектную работу',
-  'Полить цветы',
-  'Пройти туториал по Реакту',
+  'как настроить сокращения для гит',
+  'Бесплатные сервисы для анализа кроссбраузерности',
+  'Разобраться с функциями',
+  'Чистый код',
 ];
+
+let todoCount = 0;
 
 const todosListElement = document.querySelector('.todos__list');
 const todosFormElement = document.querySelector('.todos__form');
-const todosInputElement = todosFormElement.querySelector('.todos__input');
-const todosFormSubmitButton = todosFormElement.querySelector('.todos__submit-btn')
-const todoTemplateElement = document.querySelector('.todo-template');
+const todosSubmitBtn = todosFormElement.querySelector('.todos__submit-btn');
+const todosInputElement = document.querySelector('.todos__input');
+const todosTemplateElement = document.querySelector('.todo-template');
 
-let editedTodo = null;
-
-function addTodo(text) {
-  const todo = todoTemplateElement.content.cloneNode(true);
-
-  todo.querySelector('.todo__text').textContent = text;
-  addTodoListeners(todo);
-
-  todosListElement.prepend(todo);
-}
+let editedElement = null;
 
 function addTodoListeners(todo) {
-  todo.querySelector('.todo__btn_type_delete').addEventListener('click', deleteTodo);
-  todo.querySelector('.todo__btn_type_edit').addEventListener('click', editTodo);
-  todo.querySelector('.todo__btn_type_duplicate').addEventListener('click', duplicateTodo);
+  todo.querySelector('.todo__btn_type_delete').addEventListener('click', handleDelete);
+  todo.querySelector('.todo__btn_type_edit').addEventListener('click', handleEdit);
+  todo.querySelector('.todo__btn_type_duplicate').addEventListener('click', handleDuplicate);
 }
 
-function deleteTodo(e) {
-  const todo = e.target.closest('.todo');
-
+function handleDelete(evt) {
+  const todo = evt.currentTarget.closest('.todo');
   todo.remove();
 }
 
-function editTodo(e) {
-  const todo = e.target.closest('.todo');
-
-  setTodotoForm(todo);
-}
-
-function duplicateTodo(e) {
-  const todo = e.target.closest('.todo');
-
-  const newTodo = todo.cloneNode(true);
-  addTodoListeners(newTodo);
-
-  todo.after(newTodo);
-}
-
-function setTodotoForm(todo) {
-  editedTodo = todo;
-
+function setTodoToForm(todo) {
   const text = todo.querySelector('.todo__text').textContent;
   todosInputElement.value = text;
-  todosFormSubmitButton.textContent = 'Сохранить';
+  todosSubmitBtn.textContent = 'Сохранить';
 }
 
-function handleTodoFormSubmit(e) {
-  e.preventDefault();
+function resetTodoForm() {
+  editedElement = null;
+  todosInputElement.value = '';
+  todosSubmitBtn.textContent = 'Добавить';
+}
+
+function handleEdit(evt) {
+  const todo = evt.currentTarget.closest('.todo');
+
+  editedElement = todo;
+  setTodoToForm(todo);
+}
+
+function handleDuplicate(evt) {
+  const todo = evt.currentTarget.closest('.todo');
+
+  const duplicate = todo.cloneNode(true);
+
+  addTodoListeners(duplicate);
+  todo.after(duplicate);
+}
+
+function createTodo(text) {
+  const todo = todosTemplateElement.content.cloneNode(true);
+
+  todo.querySelector('.todo__text').textContent = capitalize(text);
+  addTodoListeners(todo);
+
+  return todo;
+}
+
+function addTodo(todo) {
+  todosListElement.prepend(todo);
+
+  todoCount++;
+
+  return todoCount;
+}
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
 
   const text = todosInputElement.value;
-  todosInputElement.value = '';
 
-  if (editedTodo) {
-    editedTodo.querySelector('.todo__text').textContent = text;
-    todosFormSubmitButton.textContent = 'Добавить';
-    editedTodo = null;
+  if (editedElement) {
+    editedElement.querySelector('.todo__text').textContent = text;
   } else {
-    addTodo(text);
+    const newTodo = createTodo(text);
+    const newCount = addTodo(newTodo);
+    console.log(newCount);
   }
+
+  resetTodoForm();
 }
 
 todos.forEach(text => {
-  addTodo(text);
+  const todo = createTodo(text);
+
+  const newCount = addTodo(todo);
+  console.log(newCount);
 });
 
-todosFormElement.addEventListener('submit', handleTodoFormSubmit);
+todosFormElement.addEventListener('submit', handleFormSubmit);
+
+function capitalize(text) {
+  if (!text || typeof text !== 'string') return text;
+
+  return text[0].toUpperCase() + text.slice(1);
+}
